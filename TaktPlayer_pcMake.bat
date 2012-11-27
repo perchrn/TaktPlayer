2798dcc8f1505013f4fe3199618f3f8268821ab5
unknown <pcn@.(none)>
2012-11-27 11:38:54 +0100

    Adding out of memory sign.

--- TaktPlayer_pcMake.bat
+++ TaktPlayer_pcMake.bat
@@ -1,6 +1,6 @@
 cd "c:\Users\pcn\Workspace\MusicalVideoPlayer_Player\"
-"c:\Program Files (x86)\Python27\python.exe" C:\development\pyinstaller\Makespec.py --paths gui;src -i graphics\TaktPlayer.ico PlayerMain.py
+"c:\Program Files (x86)\Python27\python.exe" C:\development\pyinstaller\Makespec.py --paths gui;src;outOfMemoryPreview.jpg -i graphics\TaktPlayer.ico PlayerMain.py
 "c:\Program Files (x86)\Python27\python.exe" C:\development\pyinstaller\Build.py -y PlayerMain.spec
 copy dist\PlayerMain\PlayerMain.exe documentation\Output\PlayerMain_debug.exe
-"c:\Program Files (x86)\Python27\python.exe" C:\development\pyinstaller\Makespec.py --paths gui;src -i graphics\TaktPlayer.ico --windowed PlayerMain.py
+"c:\Program Files (x86)\Python27\python.exe" C:\development\pyinstaller\Makespec.py --paths gui;src;outOfMemoryPreview.jpg -i graphics\TaktPlayer.ico --windowed PlayerMain.py
 "c:\Program Files (x86)\Python27\python.exe" C:\development\pyinstaller\Build.py -y PlayerMain.spec
